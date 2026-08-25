a9594030c68fe9b339b8cad07e62f25ecf2fe904
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-08-25 11:58:20 -0600

    feat: Se creó el model para la entidad del cambio de estado dla bitácora.

---
+++ CivicFix/Backend/src/models/bitacoraCambioEstado.model.ts
@@ -0,0 +1,9 @@
+export interface BitacoraCambioEstado {
+    id_bitacora: number;
+    fk_id_reporte: number;
+    fk_id_estado_anterior: number;
+    fk_id_estado_nuevo: number;
+    fk_id_empleado: number;
+    comentario: string;
+    fecha_cambio: Date;
+};
