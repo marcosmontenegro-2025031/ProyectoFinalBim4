e83955f91404aeb17667a534cdbdd57ce1effd6d
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-08-25 11:47:53 -0600

    se creo el modelo de usuario

---
+++ CivicFix/Backend/src/models/usuarios.model.ts
@@ -0,0 +1,20 @@
+export interface Usuario {
+    id_usuario?: number;
+    nombre: string;
+    apellido: string;
+    usuario: string;
+    correo: string;
+    password?: string;
+    telefono: string;
+    fecha_registro?: Date;
+}
+
+export type UserRegister = Omit<Usuario, 'id_usuario' | 'fecha_registro'>;  
+
+export type UserLogin = Pick<Usuario, 'usuario' | 'password'>;
+
+export interface JwtPayload {
+    id_usuario: number;
+    usuario: string;
+    correo: string;
+}
