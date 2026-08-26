62e9198ec92fcdf2b3068d24b3fdda906bf27099
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-08-25 18:37:06 -0600

    feat: se creo el modelo para la entidad de notificacion

---
+++ CivicFix/Backend/src/models/notificacion.model.ts
@@ -0,0 +1,8 @@
+export interface Notificacion {
+    id_notificacion: number;
+    fk_id_usuario: number;
+    fk_id_reporte: number;
+    titulo: string;
+    mensaje: string;
+    fecha_notificacion: Date;
+};
