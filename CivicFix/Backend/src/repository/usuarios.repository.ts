4b634a6b14e1650a6bc3e57ea08da7c39675ee12
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-08-26 10:09:38 -0600

    feat: se creo el service de usuarios

---
+++ CivicFix/Backend/src/repository/usuarios.repository.ts
@@ -0,0 +1,63 @@
+import { pool } from "../config/db"
+import { UserRegister,Usuario } from "../models/usuarios.model"
+import { encriptarContrasena } from "../utils/bcrypt.util"
+
+export class UsuariosRepository {
+    async obtenerUsuarios(): Promise<Usuario[]> {
+        const resultado = await pool.query('SELECT * FROM usuarios');
+        return resultado.rows;
+    }
+
+    async obtenerUsuarioPorId(id: number): Promise<Usuario | undefined> {
+        const resultado = await pool.query("SELECT * FROM usuarios WHERE id_usuario = $1",
+            [id]
+        );
+        return resultado.rows[0] as Usuario | undefined;
+    }
+
+    async crearUsuario(usuario: UserRegister): Promise<Usuario> {
+        const body : UserRegister = {
+            nombre: usuario.nombre,
+            apellido: usuario.apellido,
+            usuario: usuario.usuario,
+            correo: usuario.correo,
+            password: usuario.password,
+            telefono: usuario.telefono
+        };
+        const passwordEncriptado = await encriptarContrasena(body.password);
+        const resultado = await pool.query("INSERT INTO usuarios (nombre,apellido,usuario,correo,password,telefono) VALUES ($1,$2,$3,$4,$5,$6)",
+            [body.nombre, body.apellido, body.usuario, body.correo, passwordEncriptado, body.telefono]
+        );
+        return usuario;
+    }
+
+    async actualizarUsuario(id: number, usuario: UserRegister): Promise<Usuario | undefined> {
+        const body : UserRegister = {
+            nombre: usuario.nombre,
+            apellido: usuario.apellido,
+            usuario: usuario.usuario,
+            correo: usuario.correo,
+            password: usuario.password,
+            telefono: usuario.telefono
+        };
+        const passwordEncriptado = await encriptarContrasena(body.password);
+        const resultado = await pool.query("UPDATE usuarios SET nombre=$1, apellido=$2, usuario=$3, correo=$4, password=$5, telefono=$6 WHERE id_usuario=$7",
+            [body.nombre, body.apellido, body.usuario, body.correo, passwordEncriptado, body.telefono, id]
+        );
+        return (resultado.rowCount ?? 0)  > 0 ? usuario : undefined;
+    }
+
+    async eliminarUsuario(id: number): Promise<boolean> {
+        const resultado = await pool.query("DELETE FROM usuarios WHERE id_usuario=$1", [id]);
+        return (resultado.rowCount ?? 0) > 0;
+    }
+
+    async obtenerUsuarioPorUsuario(usuario: string): Promise<Usuario | undefined> {
+        const resultado = await pool.query(
+            "SELECT * FROM usuarios WHERE usuario = $1",
+            [usuario]
+        );
+
+        return resultado.rows[0] as Usuario | undefined;
+    }
+}
