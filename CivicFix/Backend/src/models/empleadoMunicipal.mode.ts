e7cedd6d016f2a654695b766c92c6b9771b99d1f
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-08-27 21:38:03 -0600

    feat: se creo el modelo de EmpleadoMunicipal

---
+++ CivicFix/Backend/src/models/empleadoMunicipal.mode.ts
@@ -0,0 +1,22 @@
+export interface EmpleadoMunicipal{
+    id_empleado: number;
+    nombre: string;
+    apellido: string;
+    usuario: string;
+    password: string;
+    dpi: string;
+    telefono: string;
+    correo: string;
+    cargo: string;
+    id_departamento: number;
+}
+
+export type EmpleadoRegister = Omit<EmpleadoMunicipal,'id_empleado'>
+
+export type EmpleadoLogin = Pick<EmpleadoMunicipal,'usuario' | 'password'>
+
+export interface JwPayloadEmpleado{
+    id_empleado: number;
+    usuario: string;
+    correo: string;
+}
