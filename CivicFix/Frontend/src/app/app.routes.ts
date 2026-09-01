6fea6e7c9bfd6f82999ba5fd2b5f6d508f69c416
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-08-31 19:45:10 -0600

    feat: se hicieron ligeros cambios para poder correr de manera correcta mi frontend en angular

--- CivicFix/Frontend/src/app/app.routes.ts
+++ CivicFix/Frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Asignaciones } from './components/asignaciones/asignaciones';
+import { Bitacora } from './components/bitacora/bitacora';
+import { Evidencias } from './components/evidencias/evidencias';
+import { Fotografias } from './components/fotografias/fotografias';
+import { Notificaciones } from './components/notificaciones/notificaciones';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Login },
+    { path: 'asignaciones', component: Asignaciones },
+    { path: 'bitacora', component: Bitacora },
+    { path: 'evidencias', component: Evidencias },
+    { path: 'fotografias', component: Fotografias },
+    { path: 'notificaciones', component: Notificaciones }
+];
