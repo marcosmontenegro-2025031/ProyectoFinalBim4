1693e98e7dae9f0726e826da01f9441595de8660
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-08-31 19:45:10 -0600

    feat: se hicieron ligeros cambios para poder correr de manera correcta mi frontend en angular

--- CivicFix/Frontend/src/app/app.ts
+++ CivicFix/Frontend/src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('Frontend');
+  protected readonly mostrarNav = signal(true);
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((evento): evento is NavigationEnd => evento instanceof NavigationEnd))
+      .subscribe((evento) => {
+        this.mostrarNav.set(evento.urlAfterRedirects !== '/');
+      });
+  }
 }
