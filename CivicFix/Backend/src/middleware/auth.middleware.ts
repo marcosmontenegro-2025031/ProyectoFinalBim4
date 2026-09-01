8a05c0d55d1de034ed11da8dd1a29b2c40f8d663
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-01 10:19:35 -0600

    feat: se creo el metodo de verificaccion del toquen

---
+++ CivicFix/Backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { validarTokenUsuario } from "../utils/jwt.util";
+
+export const verificarToken = (req: Request,res: Response,next: NextFunction) => {
+
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).json({
+            message:"Token no proporcionado"
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try{
+        const usuario = validarTokenUsuario(token);
+        (req as any).usuario = usuario;
+        next();
+    }catch(error){
+        return res.status(401).json({
+            message:"Token inválido"
+        });
+    }
+}
