178a9fd87406ce9238f29be9807fd012153d858b
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-09-01 11:00:11 -0600

    feat: Se creo el archivo upload.middleware.ts

--- CivicFix/Backend/src/config/upload.middleware.ts
+++ CivicFix/Backend/src/config/upload.middleware.ts
@@ -13,5 +13,5 @@ const storage = multer.diskStorage({
 
 export const upload = multer({ 
     storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 } // Límite de 5MB por imagen
+    limits: { fileSize: 5 * 1024 * 1024 } 
 });
