39df4618388f19630dbd1612220b983c0ced7b3f
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-01 11:08:24 -0600

    feat: se agrego validacion de token para empleado

--- CivicFix/Backend/src/middleware/auth.middleware.ts
+++ CivicFix/Backend/src/middleware/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import { validarTokenUsuario } from "../utils/jwt.util";
+import { validarTokenUsuario,validarTokenEmpleado } from "../utils/jwt.util";
 
-export const verificarToken = (req: Request,res: Response,next: NextFunction) => {
+export const verificarTokenUsuario = (req: Request,res: Response,next: NextFunction) => {
 
     const authHeader = req.headers.authorization;
 
@@ -16,6 +16,29 @@ export const verificarToken = (req: Request,res: Response,next: NextFunction) =>
     try{
         const usuario = validarTokenUsuario(token);
         (req as any).usuario = usuario;
+        next();
+    }catch(error){
+        return res.status(401).json({
+            message:"Token inválido"
+        });
+    }
+}
+
+export const verificarTokenEmpleado = (req: Request,res: Response,next: NextFunction) => {
+
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).json({
+            message:"Token no proporcionado"
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try{
+        const empleado = validarTokenEmpleado(token);
+        (req as any).empleado = empleado;
         next();
     }catch(error){
         return res.status(401).json({
