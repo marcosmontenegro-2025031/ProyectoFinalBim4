b4bfaeaff6492976770f98a0ef0695b8d31e6df3
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-01 08:36:29 -0600

    feat: se agregaron metodos para generar y validar tokens para empleados

--- CivicFix/Backend/src/utils/jwt.util.ts
+++ CivicFix/Backend/src/utils/jwt.util.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { JwtPayload } from '../models/usuarios.model';
+import { JwtPayloadEmpleado } from '../models/empleadoMunicipal.model';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,10 +11,18 @@ if (!CLAVE_SECRETA) {
     throw new Error("JWT_SECRET no está configurado");
 }
 
-export const generarToken = (payload: JwtPayload): string => {
+export const generarTokenUsuario = (payload: JwtPayload): string => {
   return jwt.sign(payload, CLAVE_SECRETA, { expiresIn: '4h' });
 };
 
-export const validarToken = (token: string): JwtPayload => {
+export const generarTokenEmpleado = (payload: JwtPayloadEmpleado): string => {
+  return jwt.sign(payload, CLAVE_SECRETA, { expiresIn: '4h' });
+};
+
+export const validarTokenUsuario = (token: string): JwtPayload => {
     return jwt.verify(token, CLAVE_SECRETA) as JwtPayload;
+};
+
+export const validarTokenEmpleado = (token: string): JwtPayloadEmpleado => {
+    return jwt.verify(token, CLAVE_SECRETA) as JwtPayloadEmpleado;
 };
