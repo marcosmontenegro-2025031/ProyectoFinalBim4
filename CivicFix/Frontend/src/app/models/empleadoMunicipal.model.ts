bf4256779128d4dda233a4bb8fbd44b862a9a968
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-01 11:18:06 -0600

    feat: se crearon los modelos para el frontend

---
+++ CivicFix/Frontend/src/app/models/empleadoMunicipal.model.ts
@@ -0,0 +1,12 @@
+export interface EmpleadoMunicipal{
+    id_empleado?: number;
+    nombre: string;
+    apellido: string;
+    usuario: string;
+    password: string;
+    dpi: string;
+    telefono: string;
+    correo: string;
+    cargo: string;
+    id_departamento: number;
+}
