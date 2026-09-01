269f1106900732abd5c76faa37833c1d1d3543ac
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-09-01 08:16:55 -0600

    feat: se añadio el model de bitacora

---
+++ CivicFix/Frontend/src/app/core/services/bitacoraCambioEstado.ts
@@ -0,0 +1,23 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { BitacoraCambioEstado } from '../models/bitacoraCambioEstado.model';
+
+@Injectable({ providedIn: 'root' })
+export class BitacoraCambioEstadoService {
+    private http = inject(HttpClient);
+    private baseUrl = `${environment.apiUrl}/bitacora`;
+
+    registrar(bitacora: Omit<BitacoraCambioEstado, 'id_bitacora' | 'fecha_cambio'>): Observable<BitacoraCambioEstado> {
+        return this.http.post<BitacoraCambioEstado>(this.baseUrl, bitacora);
+    }
+
+    listar(): Observable<BitacoraCambioEstado[]> {
+        return this.http.get<BitacoraCambioEstado[]>(this.baseUrl);
+    }
+
+    listarPorReporte(idReporte: number): Observable<BitacoraCambioEstado[]> {
+        return this.http.get<BitacoraCambioEstado[]>(`${this.baseUrl}/reporte/${idReporte}`);
+    }
+}
