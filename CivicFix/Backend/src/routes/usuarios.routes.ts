17d5c45b39d210bb203c0fcdb576b8dfc2e77659
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-01 11:10:49 -0600

    feat: se agrego la validacion de tokens a las rutas

--- CivicFix/Backend/src/routes/usuarios.routes.ts
+++ CivicFix/Backend/src/routes/usuarios.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import cors from "cors";
 import { UsuariosController } from "../controllers/usuarios.controller";
+import { verificarTokenUsuario,verificarTokenEmpleado } from "../middleware/auth.middleware";
 
 export const usuariosRouter = Router();
 
-usuariosRouter.get("/api/usuarios", cors(), (req, res) => {
+usuariosRouter.get("/api/usuarios", cors(), verificarTokenEmpleado, (req, res) => {
     const controller = new UsuariosController();
     controller.getUsuarios(req,res);
 });
 
-usuariosRouter.get("/api/usuarios/:id", cors(), (req, res) => {
+usuariosRouter.get("/api/usuarios/:id", cors(), verificarTokenUsuario, (req, res) => {
     const controller = new UsuariosController();
     controller.getUsuarioById(req,res);
 });
@@ -19,12 +20,12 @@ usuariosRouter.post("/api/usuarios", cors(), (req, res) => {
     controller.createUsuario(req, res);
 });
 
-usuariosRouter.put("/api/usuarios/:id", cors(), (req, res) => {
+usuariosRouter.put("/api/usuarios/:id", cors(), verificarTokenUsuario, (req, res) => {
     const controller = new UsuariosController();
     controller.updateUsuario(req, res);
 });
 
-usuariosRouter.delete("/api/usuarios/:id", cors(), (req, res) => {
+usuariosRouter.delete("/api/usuarios/:id", cors(), verificarTokenUsuario, (req, res) => {
     const controller = new UsuariosController();
     controller.deleteUsuario(req, res);
 });
