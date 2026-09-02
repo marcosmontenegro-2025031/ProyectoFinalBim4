fd71705c9e8400c102b404c6bdc14742c315732e
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-02 11:06:18 -0600

    feat: se creo la ruta de login-usuario

--- CivicFix/Frontend/src/app/app.routes.ts
+++ CivicFix/Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginUsuario } from './components/login-usuario/login-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginUsuario },
+];
