1859f54745002dc5e3fca715ac508895b92e0ba1
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-09-02 07:46:38 -0600

    feat: se añadio un mensaje al manejo de errores de postgre

--- CivicFix/Backend/src/config/db.ts
+++ CivicFix/Backend/src/config/db.ts
@@ -4,19 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT) || 5432,
-  max: 10,
-  idleTimeoutMillis: 30000 
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT) || 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
 });
 
 pool.on('connect', () => {
-  console.log('Conexión exitosa con el Pool de PostgreSQL');
+    console.log('Conexión exitosa con el Pool de PostgreSQL');
 });
 
 pool.on('error', (err) => {
-  console.error('Error inesperado en el cliente de Postgres', err);
+    console.error('Error inesperado en el cliente de Postgres:', err);
 });
