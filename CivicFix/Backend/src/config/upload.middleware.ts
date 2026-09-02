078f92bb4c5e1a5a92069f13662f63c245f3a13a
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-09-02 10:13:04 -0600

    fix: Se modifico y agrego la ruta para almacenar las evidencias

--- CivicFix/Backend/src/config/upload.middleware.ts
+++ CivicFix/Backend/src/config/upload.middleware.ts
@@ -1,17 +1,22 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
+
+const uploadDir = path.join(__dirname, '../../uploads');
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+    console.log('Carpeta "uploads" creada automáticamente.');
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
+        cb(null, `${uniqueSuffix}-${file.originalname}`);
     }
 });
 
-export const upload = multer({ 
-    storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 } 
-});
+export const upload = multer({ storage });
