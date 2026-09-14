3a554b29ebd1ca71f1587afb7209e16459c4dfef
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-14 11:56:53 -0600

    feat: se agrego el componente /register-usuario

---
+++ CivicFix/Frontend/src/app/components/register-usuario/register-usuario.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuariosService } from '../../service/usuarios.service';
+import { Router } from '@angular/router';
+
+@Component({
+  imports: [ReactiveFormsModule],
+  standalone: true,
+  selector: 'app-register-usuario',
+  styleUrl: './register-usuario.css',
+  templateUrl: './register-usuario.html',
+})
+export class RegisterUsuario {
+  RegisterForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private usuariosService: UsuariosService,
+    private router: Router
+  ){
+    this.RegisterForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      usuario: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required, Validators.minLength(8)],
+      telefono: ['', Validators.required]
+    })
+  }
+
+  mostarPassword = false;
+
+  tooglePassword() {
+    this.mostarPassword = !this.mostarPassword;
+  }
+
+  onSumit() {
+    if (this.RegisterForm.valid) {
+      this.usuariosService.crearUsuario(this.RegisterForm.value).subscribe({
+        next: (response) => {
+          console.log('Usuario registrado exitosamente.');
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          console.error('STATUS:', err.status);
+          console.error('ERROR:', err.error);
+          console.error('MENSAJE:', err.message);
+
+          alert(JSON.stringify(err.error));
+        }
+      })
+    }
+  }
+}
