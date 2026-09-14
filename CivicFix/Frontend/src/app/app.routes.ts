c14b0f4ebcbd94ec926a2789211307545c827943
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-14 11:58:35 -0600

    feat: se agrego el url register-usuario

--- CivicFix/Frontend/src/app/app.routes.ts
+++ CivicFix/Frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginUsuario } from './components/login-usuario/login-usuario.component';
+import { RegisterUsuario } from './components/register-usuario/register-usuario.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginUsuario },
+    { path: 'register',component: RegisterUsuario },
 ];
