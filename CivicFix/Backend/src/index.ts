bf305938b3fe1bc11f50f3e123aa2a7cd83f52c9
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-09-15 17:12:36 -0600

    feat: se añadio el express cors

---
+++ CivicFix/Backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import { asignacionRouter } from "./routes/asignacion.routes";
+import { bitacoraCambioEstadoRouter } from "./routes/bitacoraCambioEstado.routes";
+import { evidenciaSolucionRouter } from "./routes/evidenciaSolucion.routes";
+import { fotoProblemaRouter } from "./routes/fotoProblema.routes";
+import { notificacionRouter } from "./routes/notificacion.routes";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(asignacionRouter);
+app.use(bitacoraCambioEstadoRouter);
+app.use(evidenciaSolucionRouter);
+app.use(fotoProblemaRouter);
+app.use(notificacionRouter);
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
