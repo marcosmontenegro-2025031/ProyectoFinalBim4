4e1a77265ef9d98af85981b21340b3ab190c8863
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-09-15 17:17:00 -0600

    feat: se elimino el login-

--- CivicFix/Frontend/src/app/app.routes.ts
+++ CivicFix/Frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { Login } from './components/login/login';
 import { Asignaciones } from './components/asignaciones/asignaciones';
 import { Bitacora } from './components/bitacora/bitacora';
 import { Evidencias } from './components/evidencias/evidencias';
@@ -7,7 +6,6 @@ import { Fotografias } from './components/fotografias/fotografias';
 import { Notificaciones } from './components/notificaciones/notificaciones';
 
 export const routes: Routes = [
-    { path: '', component: Login },
     { path: 'asignaciones', component: Asignaciones },
     { path: 'bitacora', component: Bitacora },
     { path: 'evidencias', component: Evidencias },
