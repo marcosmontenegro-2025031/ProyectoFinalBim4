090bee2f4d299117883c38b97d7a8d447bc90084
jaquino-2025376 <jaquino-2025376@kinal.edu.gt>
2026-09-15 17:14:42 -0600

    feat: se cambiaron los nombres de la url

--- CivicFix/Backend/src/routes/fotoProblema.routes.ts
+++ CivicFix/Backend/src/routes/fotoProblema.routes.ts
@@ -4,32 +4,32 @@ import { FotoProblemaController } from "../controllers/fotoProblema.controller.j
 
 export const fotoProblemaRouter = Router();
 
-fotoProblemaRouter.get("/api/fotos-problema", cors(), (req, res) => {
+fotoProblemaRouter.get("/api/fotografias", cors(), (req, res) => {
     const controller = new FotoProblemaController();
     controller.listar(req, res);
 });
 
-fotoProblemaRouter.get("/api/fotos-problema/reporte/:idReporte", cors(), (req, res) => {
+fotoProblemaRouter.get("/api/fotografias/reporte/:idReporte", cors(), (req, res) => {
     const controller = new FotoProblemaController();
     controller.obtenerPorReporte(req, res);
 });
 
-fotoProblemaRouter.get("/api/fotos-problema/:id", cors(), (req, res) => {
+fotoProblemaRouter.get("/api/fotografias/:id", cors(), (req, res) => {
     const controller = new FotoProblemaController();
     controller.obtenerPorId(req, res);
 });
 
-fotoProblemaRouter.post("/api/fotos-problema", cors(), (req, res) => {
+fotoProblemaRouter.post("/api/fotografias", cors(), (req, res) => {
     const controller = new FotoProblemaController();
     controller.crear(req, res);
 });
 
-fotoProblemaRouter.put("/api/fotos-problema/:id", cors(), (req, res) => {
+fotoProblemaRouter.put("/api/fotografias/:id", cors(), (req, res) => {
     const controller = new FotoProblemaController();
     controller.actualizar(req, res);
 });
 
-fotoProblemaRouter.delete("/api/fotos-problema/:id", cors(), (req, res) => {
+fotoProblemaRouter.delete("/api/fotografias/:id", cors(), (req, res) => {
     const controller = new FotoProblemaController();
     controller.eliminar(req, res);
 });
