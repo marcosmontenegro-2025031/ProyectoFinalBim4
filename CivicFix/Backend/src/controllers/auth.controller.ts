374369bfe12e5c811df2a6dc728cdd5f83345ad8
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-15 22:00:24 -0600

    feat: se agrego un console.log para reflejar el login en consola

--- CivicFix/Backend/src/controllers/auth.controller.ts
+++ CivicFix/Backend/src/controllers/auth.controller.ts
@@ -7,6 +7,7 @@ export class AuthController {
     async loginUsuario(req: Request, res: Response){
         try{
             const respuesta = await this.service.loginUsuario(req.body);
+            console.log('Login exitoso. El token ya se guardó.', respuesta);
             res.status(200).json(respuesta);
         }catch(error:any){
             res.status(401).json({message:error.message});
