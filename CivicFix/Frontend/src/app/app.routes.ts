4f6c01fa426385940b670e3c57e4a60db6aaa088
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-16 11:33:37 -0600

    refactor: se actualizaron dependencia y modificaron configuraciones

--- CivicFix/Frontend/src/app/app.routes.ts
+++ CivicFix/Frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginUsuario } from './components/login-usuario/login-usuario.component';
 import { RegisterUsuario } from './components/register-usuario/register-usuario.component';
-import { EmpleadoLogin } from './empleado-login/empleado-login.component';
-import { EmpleadoRegister } from './empleado-register/empleado-register.component';
+import { EmpleadoLogin } from './components/empleado-login/empleado-login.component';
+import { EmpleadoRegister } from './components/empleado-register/empleado-register.component';
+import { FormularioRedireccional } from './components/formulario-redireccional/formulario-redireccional.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '', redirectTo: '/formulario', pathMatch: 'full' },
+    { path: 'formulario', component: FormularioRedireccional },
     { path: 'login', component: LoginUsuario },
     { path: 'register',component: RegisterUsuario },
     { path: 'empleado-login', component: EmpleadoLogin },
