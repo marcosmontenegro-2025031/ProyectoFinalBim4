91940458438732860e1916fb6852bbcbef1390d8
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-15 22:49:28 -0600

    feat:se agrego la ruta para empleado-login

--- CivicFix/Frontend/src/app/app.routes.ts
+++ CivicFix/Frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginUsuario } from './components/login-usuario/login-usuario.component';
 import { RegisterUsuario } from './components/register-usuario/register-usuario.component';
+import { EmpleadoLogin } from './empleado-login/empleado-login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginUsuario },
     { path: 'register',component: RegisterUsuario },
+    { path: 'empleado-login', component: EmpleadoLogin },
 ];
