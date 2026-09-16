db43931bf317d11ade6e67d75c1b80bfddb4b763
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-15 22:51:03 -0600

    feat: se agrego el componente emplesos-login

---
+++ CivicFix/Frontend/src/app/empleado-login/empleado-login.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EmpleadoLogin } from './empleado-login';
+
+describe('EmpleadoLogin', () => {
+  let component: EmpleadoLogin;
+  let fixture: ComponentFixture<EmpleadoLogin>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EmpleadoLogin],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EmpleadoLogin);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
