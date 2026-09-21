d2272e9d079d9e99d47f6b1ed1b3c1b759c577cd
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-09-20 22:57:50 -0600

    feat:Se ha modificado vistas mapa

--- CivicFix/Frontend/src/app/app.routes.ts
+++ CivicFix/Frontend/src/app/app.routes.ts
@@ -2,28 +2,15 @@ import { Routes } from '@angular/router';
 
 import { MisReportesComponent } from './components/mis-reportes/mis-reportes.component';
 import { ReporteComponent } from './components/reporte/reporte.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+
+import { NotificacionesComponent } from './components/notificaciones/notificaciones';
 
 export const routes: Routes = [
-
-  {
-    path: 'reportes',
-    component: MisReportesComponent
-  },
-
-  {
-    path: 'reportes/nuevo',
-    component: ReporteComponent
-  },
-
-  {
-    path: '',
-    redirectTo: 'reportes',
-    pathMatch: 'full'
-  },
-
-  {
-    path: '**',
-    redirectTo: 'reportes'
-  }
-
+  { path: 'reportes/nuevo', component: ReporteComponent },
+  { path: 'reportes/mis-reportes', component: MisReportesComponent },
+  { path: 'mapa', component: MapaComponent },
+  { path: 'notificaciones', component: NotificacionesComponent },
+  { path: '', redirectTo: 'reportes/nuevo', pathMatch: 'full' },
+  { path: '**', redirectTo: 'reportes/nuevo' }
 ];
