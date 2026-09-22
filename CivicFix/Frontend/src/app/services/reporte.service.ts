25f6ca116f3144a15344a136e04ef3f00011ccfb
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-21 20:45:45 -0600

    feat: se agrego validacion de toquen a la ruta para obtener empleado

--- CivicFix/Frontend/src/app/services/reporte.service.ts
+++ CivicFix/Frontend/src/app/services/reporte.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CrearReporteDTO, RespuestaReporte, PuntoMapa } from '../models/reporte.model';
 import { UsuariosService } from './usuarios.service';
+import { EmpleadoService } from './empleado.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class ReporteService {
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:3000/api/reportes';
 
-  constructor(private usuariosService: UsuariosService) {}
+  constructor(private usuariosService: UsuariosService, private empleadoService: EmpleadoService) {}
 
   registrarReporte(dto: CrearReporteDTO): Observable<RespuestaReporte> {
     return this.http.post<RespuestaReporte>(this.apiUrl, dto);
@@ -36,7 +37,12 @@ export class ReporteService {
   }
 
   obtenerTodosLosReportes(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    const token = this.empleadoService.obtenerToken();
+
+    const headers = new HttpHeaders({
+        Authorization: `Bearer ${token}`
+    });
+    return this.http.get<any[]>(this.apiUrl, { headers });
   }
 
   actualizarEstado(idReporte: number, idEstado: number): Observable<{ mensaje: string }> {
