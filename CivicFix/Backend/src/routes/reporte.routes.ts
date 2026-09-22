57fb1d4d719418649ebd44bb0a45c2bfe6ce7254
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-09-21 20:43:04 -0600

    feat: se agrego validacion de toquen a la ruta para obtener empleado

--- CivicFix/Backend/src/routes/reporte.routes.ts
+++ CivicFix/Backend/src/routes/reporte.routes.ts
@@ -5,12 +5,12 @@ import {
   obtenerPuntosMapaHandler, 
   obtenerReportesHandler // <--- 1. Importa el controlador para listar reportes
 } from '../controllers/reporte.controller';
-import { verificarTokenUsuario } from '../middleware/auth.middleware';
+import { verificarTokenUsuario, verificarTokenEmpleado } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.post('/reportes', crearReporteHandler);
-router.get('/reportes', obtenerReportesHandler);    
+router.get('/reportes', verificarTokenEmpleado, obtenerReportesHandler);
 router.get('/reportes/mapa', obtenerPuntosMapaHandler);
 router.get(
     '/reportes/mis-reportes',
